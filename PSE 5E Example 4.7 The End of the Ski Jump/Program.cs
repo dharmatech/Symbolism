7531f1181ef0aacaf02d8ada0723a6f7b0016b43
Eduardo Cavazos <wayo.cavazos@gmail.com>
2013-03-09 12:49:53 -0600

    Edit PSE Example 4.7

--- PSE 5E Example 4.7 The End of the Ski Jump/Program.cs
+++ PSE 5E Example 4.7 The End of the Ski Jump/Program.cs
@@ -18,12 +18,15 @@ namespace PSE_5E_Example_4._7_The_End_of_the_Ski_Jump
             // The landing incline below him falls off with a slope of 35.0°. 
             // Where does he land on the incline?
 
-            var thA = new Symbol("thA"); // angle at point A
-            var vA = new Symbol("vA"); // velocity at point A
+            var thA = new Symbol("thA");    // angle at point A
+            
+            var vA = new Symbol("vA");      // velocity at point A
 
-            var g = new Symbol("g"); // magnitude of gravity
+            var g = new Symbol("g");        // magnitude of gravity
 
-            var _g = new Point(0, -g); // gravity vector
+            var _g = new Point(0, -g);      // gravity vector
+
+            var th = new Symbol("th");      // angle of incline
 
             var objA = new Obj()
             {
@@ -33,15 +36,19 @@ namespace PSE_5E_Example_4._7_The_End_of_the_Ski_Jump
                 time = 0
             };
 
-            var th = new Symbol("th"); // angle of incline
+            Func<MathObject, MathObject> numeric = obj =>
+                obj
+                    .Substitute(vA, 25)
+                    .Substitute(thA, 0.0)
+                    .Substitute(th, (-35).ToRadians())
+                    .Substitute(Trig.Pi, 3.14159)
+                    .Substitute(g, 9.8);
 
             var intersection = objA.ProjectileInclineIntersection(th);
 
             Action nl = () => "".Disp();
 
-            "Where does he land on the incline?".Disp();
-
-            "".Disp();
+            "Where does he land on the incline?".Disp(); nl();
 
             "x position (symbolic):".Disp();
 
@@ -53,27 +60,11 @@ namespace PSE_5E_Example_4._7_The_End_of_the_Ski_Jump
 
             "x position (numeric):".Disp();
 
-            intersection.x
-                .Substitute(vA, 25)
-                .Substitute(thA, 0.0)
-                .Substitute(th, (-35).ToRadians())
-                .Substitute(Trig.Pi, 3.14159)
-                .Substitute(g, 9.8)
-                .Disp();
-
-            nl();
+            numeric(intersection.x).Disp(); nl();
 
             "y position (numeric):".Disp();
 
-            intersection.y
-                .Substitute(vA, 25)
-                .Substitute(thA, 0.0)
-                .Substitute(th, (-35).ToRadians())
-                .Substitute(Trig.Pi, 3.14159)
-                .Substitute(g, 9.8)
-                .Disp();
-
-            nl();
+            numeric(intersection.y).Disp(); nl();
 
             var objB = new Obj()
             {
@@ -81,34 +72,21 @@ namespace PSE_5E_Example_4._7_The_End_of_the_Ski_Jump
                 acceleration = _g
             };
 
-            "Determine how long the jumper is airborne".Disp();
-
-            nl();
+            "Determine how long the jumper is airborne".Disp(); nl();
 
             "symbolic:".Disp();
 
             var timeB = Calc.Time(objA, objB, 1);
 
-            timeB.Disp();
-
-            nl();
+            timeB.Disp(); nl();
 
             "numeric:".Disp();
 
-            timeB
-                .Substitute(vA, 25)
-                .Substitute(thA, 0.0)
-                .Substitute(th, (-35).ToRadians())
-                .Substitute(Trig.Pi, 3.14159)
-                .Substitute(g, 9.8)
-                .Disp();
-
-            nl();
+            numeric(timeB).Disp(); nl();
 
             objB = objA.AtTime(timeB);
 
             "Determine his vertical component of velocity just before he lands".Disp();
-
             nl();
 
             "symbolic:".Disp();
@@ -117,13 +95,7 @@ namespace PSE_5E_Example_4._7_The_End_of_the_Ski_Jump
 
             "numeric:".Disp();
 
-            objB.velocity.y
-                .Substitute(vA, 25)
-                .Substitute(thA, 0.0)
-                .Substitute(th, (-35).ToRadians())
-                .Substitute(Trig.Pi, 3.14159)
-                .Substitute(g, 9.8)
-                .Disp();
+            numeric(objB.velocity.y).Disp();
 
             Console.ReadLine();
         }
--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -12,5 +12,7 @@ namespace Utils
             Console.WriteLine(obj);
             return obj;
         }
+
+        
     }
 }
