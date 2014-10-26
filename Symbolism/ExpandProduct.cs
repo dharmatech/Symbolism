792d9ba545e57679e64d115d67a1049612d35b40
dharmatech <wayo.cavazos@gmail.com>
2014-10-26 14:34:46 -0500

    ExpandProduct

---
+++ Symbolism/ExpandProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Symbolism
+{
+    namespace ExpandProduct
+    {
+        public static class Extensions
+        {
+            public static MathObject ExpandProduct(this MathObject r, MathObject s)
+            {
+                if (r is Sum)
+                {
+                    var f = (r as Sum).elts[0];
+
+                    return f.ExpandProduct(s) + (r - f).ExpandProduct(s);
+                }
+
+                if (s is Sum) return s.ExpandProduct(r);
+
+                return r * s;
+            }
+        }
+    }
+}
